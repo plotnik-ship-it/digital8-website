1848fad8ec86cbbca649be10fcec2560275b7c30
plotnik <plotnik@gmail.com>
2026-01-06 16:29:30 +0000

    Fix: Upgrade lenis dependency to resolve build error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  output: "static",
   integrations: [tailwind(), react()],
-  adapter: node({
-    mode: "standalone"
-  })
+  vite: {
+    ssr: {
+      noExternal: ["gsap"],
+    },
+  },
 });
